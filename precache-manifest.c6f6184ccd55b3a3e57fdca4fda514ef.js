2b5de8fced81dce2f698243cfe2581b986b36c4c
junmile <designeryjun@gmail.com>
2020-04-08 19:44:48 +0900

    Updates

---
+++ precache-manifest.c6f6184ccd55b3a3e57fdca4fda514ef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a58bdf336411fef480cb745cfaa9a80e",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "1d492f4218a8c420d610",
+    "url": "/movie_app_2020/static/css/main.253e4b87.chunk.css"
+  },
+  {
+    "revision": "2731f645873efd1058d0",
+    "url": "/movie_app_2020/static/js/2.a7e02d2d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app_2020/static/js/2.a7e02d2d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d492f4218a8c420d610",
+    "url": "/movie_app_2020/static/js/main.4275fe63.chunk.js"
+  },
+  {
+    "revision": "8b67d7ccc50a17c9e597",
+    "url": "/movie_app_2020/static/js/runtime-main.aaeb0f22.js"
+  }
+]);
